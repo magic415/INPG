071bfb6bb34026a5a0cbec4e96d93b113ba9c6c3
lukasy009 <lukaszbednarski12345@gmail.com>
2017-04-10 16:35:58 +0200

    dynamiczna alokacja pamieci

---
+++ Kody/dynamiczna laokacja .c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int* tablica(int l)
+{
+    int *tab = malloc(l*sizeof(*tab)),i;
+        if(tab!=NULL)
+        {
+            for(i=0;i<l;i++)
+                tab[i] = (2*i+1);
+            return tab;
+        }
+        return NULL;
+}
+
+int main(){
+    int *wsk,i,x;
+    scanf("%d",&x);
+    wsk = tablica(x);
+    if(wsk!=NULL)
+    for(i = 0;i<x;i++)
+        printf("%d ",wsk[i]);
+
+ free (wsk);
+return 0;
+}
