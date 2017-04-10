203aab8190b73bafa295fda5bb0f7b12e764e63e
lukasy009 <lukaszbednarski12345@gmail.com>
2017-04-10 16:28:40 +0200

    Dodanie Wariancji i zmiana rozszerzen

---
+++ Kody/Wariancja .c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define R 5
+int main()
+{
+    float tab[R], j=0,k=0;
+    int i;
+    printf("podaj piêæ liczb, których œredni¹ i wariacje chcesz policzyæ\n");
+
+    for(i = 0;i<R;i++)
+        {
+            scanf("%f",&tab[i]);
+            j+=tab[i];
+        }
+    j = j/R;
+    printf("Œrednia arytmetyczna wynosi: %f\n",j);
+
+    for(i = 0;i<R;i++)
+        k+=pow((tab[i]-j),2);
+    k=k/R;
+    printf("Wariacja wynosi %f",k);
+
+
+    return 0;
+}
