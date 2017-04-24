2c3d7e33ad868e972e8b9c2d3f78e2f869662f51
magic415 <magic.4155@gmail.com>
2017-04-24 16:02:04 +0200

    up

---
+++ Kody/prog1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+<<<<<<< HEAD
+int* tablica(int l)///konflikt
+{
+=======
+int* tablica(int l)
+{                       //konflikt
+>>>>>>> bef6a328955b4589b92a6162c86b340cd71d1d92
+    int *tab = malloc(l*sizeof(*tab)),i;
+        if(tab!=NULL)
+        {
+            for(i=0;i<l;i++)
+                tab[i] = (2*i+1);
+            return tab;
+        }
+        return NULL;
+}
+
+int main(){
+    int *wsk,i,x;
+    scanf("%d",&x);
+    wsk = tablica(x);
+    if(wsk!=NULL)
+    for(i = 0;i<x;i++)
+        printf("%d ",wsk[i]);
+
+ free (wsk);
+return 0;
+}
